c5d87514a9f1a02e376186a3227d7c42ab1aa11a
achmad-syidiq <achmadsyidiq@outlook.com>
2025-10-24 14:03:34 +0700

    Register The Blog Blueprint

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -21,9 +21,12 @@ def create_app(test_config=None):
 
     from . import db
     from . import auth
+    from . import blog
 
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     # simple page that says hello
     @app.route('/hello')
